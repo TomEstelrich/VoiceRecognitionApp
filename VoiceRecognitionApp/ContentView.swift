5f466cab8829219ebbd3be31336610063298949c
Tom Estelrich <tom.estelrich@thebitsfactory.com>
2023-09-16 11:17:18 +0200

    Project files created

---
+++ VoiceRecognitionApp/ContentView.swift
@@ -0,0 +1,19 @@
+// 16.09.23 | VoiceRecognitionApp - ContentView.swift | Tom Estelrich
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceRecognitionApp/VoiceRecognitionAppApp.swift
@@ -0,0 +1,12 @@
+// 16.09.23 | VoiceRecognitionApp - VoiceRecognitionAppApp.swift | Tom Estelrich
+
+import SwiftUI
+
+@main
+struct VoiceRecognitionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
